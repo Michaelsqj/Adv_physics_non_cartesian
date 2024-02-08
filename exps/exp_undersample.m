4c7b2196bee71fb68706a1791dcc404a72365e99
Qijia Shen <dcs094@rescomp2.hpc.in.bmrc.ox.ac.uk>
2024-02-08 02:03:18 +0000

    add more trajectory

--- exps/exp_undersample.m
+++ exps/exp_undersample.m
@@ -18,7 +18,9 @@ kdata = fftshift(fft2(img));
 
 %%
 kdata_ud = kdata;
-kdata_ud(1:3:end,:)=0;
+kdata_ud(1:4:end,:)=0;
+kdata_ud(2:4:end,:)=0;
+kdata_ud(3:4:end,:)=0;
 img_ud = ifft2(fftshift(kdata_ud));
 
 %%
@@ -29,14 +31,35 @@ img(im_size/2, im_size/2) = 1;
 kdata = fftshift(fft2(img));
 % kdata_ud = kdata(1:3:end,:);
 kdata_ud = kdata;
-kdata_ud(1:3:end,:)=0;
+kdata_ud(4:4:end,:)=0;
+kdata_ud(2:4:end,:)=0;
+kdata_ud(3:4:end,:)=0;
 img_ud = ifft2(fftshift(kdata_ud));
 
 
 %%
-p.n=128;
-p.nspokes=40;
+p.n=512;
+p.nspokes=64;
 kspace=radial_2d(p);
 
-E = xfm_NUFFT([128,128,1,1],[],[],reshape(kspace,[],1,2));  % no density compensation
-% img_ud = E.mtimes2(img);
+E = xfm_NUFFT([512,512,1,1],[],[],reshape(kspace,[],1,2)); 
+img_ud = E.mtimes2(img);
+figure;
+imshow(abs(img_ud),[]);
+delta_in= zeros(p.n);
+delta_in(p.n/2+1,p.n/2+1)=1;
+ psf=fftshift(ifft2(E.PSF(:,:,1)));
+% psf_k=fftshift(fft2(psf));
+figure;
+imshow(abs(psf),[0,0.001*max(abs(psf(:)))])
+% imshow(abs(psf_k),[])
+
+%%
+kspace=spiral_2d();
+E = xfm_NUFFT([512,512,1,1],[],[],reshape(kspace,[],1,2)); 
+img_ud = E.mtimes2(img);
+psf=fftshift(ifft2(E.PSF(:,:,1)));
+figure;
+imshow(abs(img_ud),[]);
+figure;
+imshow(abs(psf),[0,0.01*max(abs(psf(:)))])
--- plot/plot_trajectory.m
+++ plot/plot_trajectory.m
@@ -8,42 +8,68 @@ function plot_trajectory(kspace)
 %     kspace =  kspace;
     Nshots = size(kspace,1);
     Ndim = size(kspace,3);
+    kspace = kspace ./ 1.2;
         % set axis equal
-    
+    figure;
+    drawArrow = @(x,y) quiver( x(2),y(2),x(2)-x(1),y(2)-y(1),0 );
     for i = 1:Nshots
         if Ndim == 2
-            plot(kspace(i,:,1),kspace(i,:,2),'LineWidth',2,'Marker','.','MarkerSize',10);            
+            % specify the color using colormap
+            
+            plot(kspace(i,:,1),kspace(i,:,2),'LineWidth',2,'Marker','.','MarkerSize',5);      
+%             line2arrow(h);
+            hold on;
+%             drawArrow(squeeze(kspace(i,end-1:end,1)),squeeze(kspace(i,end-1:end,2)));
+%             arrow3([squeeze(kspace(i,end-1,1)),squeeze(kspace(i,end-1,2))],[squeeze(kspace(i,end,1)),squeeze(kspace(i,end,2))]);
+            hold on;
         elseif Ndim == 3
-            plot3(kspace(i,:,1),kspace(i,:,2),kspace(i,:,3),'LineWidth',2,'Marker','.','MarkerSize',10);
+            plot3(kspace(i,:,1),kspace(i,:,2),kspace(i,:,3),'LineWidth',4);
+            hold on;
         end
         hold on;
     end
     % add arrowed lines to show the axes crossing zero
     % add arrow to each axis
     if Ndim == 2
-        h1 = plot([-1 1],[0 0],'k','LineWidth',1.5);
-        h2 = plot([0 0],[-1 1],'k','LineWidth',1.5);
-        line2arrow(h1);
-        line2arrow(h2);
+%         h1 = plot([-1 1],[0 0],'k','LineWidth',1.5);
+%         hold on;
+%         h2 = plot([0 0],[-1 1],'k','LineWidth',1.5);
+%         hold on;
+% %         line2arrow(h1);
+%         arrow3([-1 0],[1,0])
+%             hold on;
+% %         line2arrow(h2);
+% %         vectarrow([0,0],[-1,1])
+%         arrow3([0,-1],[0,1]);
+        hold on;
     elseif Ndim == 3
-        h = plot3([-1 1],[0 0],[0 0],'k','LineWidth',1.5);
-        line2arrow(h);
-        h = plot3([0 0],[-1 1],[0 0],'k','LineWidth',1.5);
-        line2arrow(h);
-        h = plot3([0 0],[0 0],[-1 1],'k','LineWidth',1.5);
-        line2arrow(h);
+        h = plot3([-1 1],[0 0],[0 0],'k','LineWidth',2);
+        hold on;
+        arrow3([-1 0 0],[1,0,0]);
+        hold on;
+%         line2arrow(h);
+        h = plot3([0 0],[-1 1],[0 0],'k','LineWidth',2);
+        hold on;
+%         line2arrow(h);
+        arrow3([0,-1,0],[0,1,0]);
+        hold on;
+        h = plot3([0 0],[0 0],[-1 1],'k','LineWidth',2);
+        hold on;
+        arrow3([0,0,-1],[0,0,1]);
+        hold on;
+%         line2arrow(h);
     end
 
     % plot x, y grid in light grey color in 2D in the background
-    if Ndim == 2
-        for i = -1:0.2:1
-            plot([i i],[-1 1],'Color',[0.8 0.8 0.8]);
-            plot([-1 1],[i i],'Color',[0.8 0.8 0.8]);
-        end
-    end
+%     if Ndim == 2
+%         for i = -1:0.2:1
+%             plot([i i],[-1 1],'Color',[0.8 0.8 0.8]);
+%             plot([-1 1],[i i],'Color',[0.8 0.8 0.8]);
+%         end
+%     end
 
     % turn on grid
-    grid on;
+    grid off;
     % set axis equal
     axis equal;
     % turn of axis, and ticklabels
